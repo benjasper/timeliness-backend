9457385a2322e92c14525e2de808adc83e555893
Benjamin Jasper <jasper.benjamin@outlook.de>
2020-12-26 16:29:08 +0100

    added refreshed token caching

--- pkg/tasks/planning_controller.go
+++ pkg/tasks/planning_controller.go
@@ -1,20 +1,32 @@
 package tasks
 
 import (
+	"context"
 	"github.com/timeliness-app/timeliness-backend/pkg/tasks/calendar"
 	"github.com/timeliness-app/timeliness-backend/pkg/users"
 )
 
 type PlanningController struct {
-	repository calendar.RepositoryInterface
+	repository  calendar.RepositoryInterface
+	userService *users.UserService
 }
 
-func NewPlanningController(u *users.User) (*PlanningController, error) {
+func NewPlanningController(u *users.User, userService *users.UserService) (*PlanningController, error) {
 	controller := PlanningController{}
+	controller.userService = userService
 	// TODO: Figure out which repository to use
-	calendarRepository, err := calendar.NewCalendarRepository(u)
+	oldAccessToken := u.GoogleCalendarConnection.Token.AccessToken
+	calendarRepository, err := calendar.NewGoogleCalendarRepository(u)
 	if err != nil {
 		return nil, err
+	}
+
+	if oldAccessToken != u.GoogleCalendarConnection.Token.AccessToken {
+		println("Refreshed Google access Token")
+		err := userService.Update(context.TODO(), u)
+		if err != nil {
+			return nil, err
+		}
 	}
 
 	controller.repository = calendarRepository
