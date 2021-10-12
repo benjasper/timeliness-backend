f7d9c7000a83cdd31bcb997407763f1c0ef95040
Benjamin Jasper <jasper.benni@gmail.com>
2021-10-12 14:07:12 +0200

    fix: fixed missing comments

--- pkg/environment/environment.go
+++ pkg/environment/environment.go
@@ -1,5 +1,10 @@
 package environment
 
+// Production defines the prod environment
 const Production = "prod"
+
+// Staging defines the staging environment
 const Staging = "staging"
+
+// Dev defines the dev environment
 const Dev = "dev"
